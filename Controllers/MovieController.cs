9567e3d377cfa92ed23748db420fbf9f544f9ab3
Jyri <jyri.poytalaakso@hotmail.com>
2025-05-10 23:45:56 +0300

    Näytetään 3 elokuvaa yhden sijasta

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -24,26 +24,48 @@ namespace MovieRoulette.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetRandomMovie(string genre)
+        public async Task<IActionResult> GetRandomMovies(string genre)
         {
             var searchUrl = $"http://www.omdbapi.com/?apikey={_apiKey}&s={genre}&type=movie";
             var searchResponse = await _httpClient.GetStringAsync(searchUrl);
             var searchJson = JsonDocument.Parse(searchResponse);
 
-            if (!searchJson.RootElement.TryGetProperty("Search", out var movies))
+            if (!searchJson.RootElement.TryGetProperty("Search", out var movies) || movies.GetArrayLength() < 3)
             {
-                return Json(new { error = "No movies found." });
+                return Json(new { error = "Not enough movies found." });
             }
 
-            var random = new Random().Next(movies.GetArrayLength());
-            var selected = movies[random];
-            var imdbID = selected.GetProperty("imdbID").GetString();
+            var random = new Random();
+            var movieList = movies.EnumerateArray().ToList();
+            int n = movieList.Count;
 
-            var detailUrl = $"http://www.omdbapi.com/?apikey={_apiKey}&i={imdbID}";
-            var detailResponse = await _httpClient.GetStringAsync(detailUrl);
-            var movie = JsonSerializer.Deserialize<Movie>(detailResponse);
+            // API palauttaa ennakoitavat tulokset, joten luodaan satunnaisuutta
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                var temp = movieList[i];
+                movieList[i] = movieList[j];
+                movieList[j] = temp;
+            }
 
-            return Json(movie);
+            var selectedMovies = new List<string>();
+            for (int i = 0; i < 3 && i < movieList.Count; i++)
+            {
+                selectedMovies.Add(movieList[i].GetProperty("imdbID").GetString());
+            }
+
+            var finalMovies = new List<Movie>();
+
+            foreach (var imdbID in selectedMovies)
+            {
+                var detailUrl = $"http://www.omdbapi.com/?apikey={_apiKey}&i={imdbID}";
+                var detailResponse = await _httpClient.GetStringAsync(detailUrl);
+                var movie = JsonSerializer.Deserialize<Movie>(detailResponse);
+                if (movie != null)
+                    finalMovies.Add(movie);
+            }
+
+            return Json(finalMovies);
         }
     }
 }
